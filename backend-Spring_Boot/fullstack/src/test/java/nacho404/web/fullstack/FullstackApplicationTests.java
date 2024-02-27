420b99eb3ff113e7b41b305fad372bda84bfe0b0
Nacho6666 <111170094+Nacho6666@users.noreply.github.com>
2024-02-27 17:54:53 +0800

    backend: basic setup

---
+++ backend-Spring_Boot/fullstack/src/test/java/nacho404/web/fullstack/FullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package nacho404.web.fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
