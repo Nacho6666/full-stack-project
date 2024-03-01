133dc6321378e06f69c6a4beafef389d4291541a
Nacho6666 <111170094+Nacho6666@users.noreply.github.com>
2024-03-02 06:26:50 +0800

    front-end: login and logout setting

--- frontend-Angular/src/app/content/content.component.ts
+++ frontend-Angular/src/app/content/content.component.ts
@@ -2,17 +2,26 @@ import { Component } from '@angular/core';
 import { WelcomeContentComponent } from '../welcome-content/welcome-content.component';
 import { LoginFormComponent } from '../login-form/login-form.component';
 import { AxiosService } from '../axios.service';
+import { ButtonsComponent } from "../buttons/buttons.component";
+import { AuthContentComponent } from "../auth-content/auth-content.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-content',
-  standalone: true,
-  imports: [WelcomeContentComponent, LoginFormComponent],
-  templateUrl: './content.component.html',
-  styleUrl: './content.component.css'
+    selector: 'app-content',
+    standalone: true,
+    templateUrl: './content.component.html',
+    styleUrl: './content.component.css',
+    imports: [WelcomeContentComponent, LoginFormComponent, ButtonsComponent, AuthContentComponent, CommonModule]
 })
 export class ContentComponent {
 
+  componentToShow: string = "welcome"
+
   constructor(private axiosService: AxiosService) {}
+
+  showComponent(componentToShow: string): void {
+    this.componentToShow = componentToShow;
+  }
 
   onLogin(input: any) {
     this.axiosService.request(
